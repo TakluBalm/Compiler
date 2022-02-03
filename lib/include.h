b55b8b68dd4d92aebd67e82500294605f4fd2cc5
TakluBalm <jugalchap123@gmail.com>
2022-02-03 02:06:49 +0000

    Compressed all include files into one include to reduce mess in the code

---
+++ lib/include.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "include/fio.h"
+#include "include/graph.h"
+#include "include/hash_table.h"
+
+#endif
---
+++ src/include.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_SRC_H
+#define INCLUDE_SRC_H
+
+#include "../lib/include.h"
+#include "include/lexer.h"
+#include "include/symbol.h"
+
+#endif
