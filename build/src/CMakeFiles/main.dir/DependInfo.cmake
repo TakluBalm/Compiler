9be2cd3bbe7e777eabdf9e997586ebdf9674bac3
Jugal <jugalchap123@gmail.com>
2023-10-23 18:53:36 +0530

    Massive Commit
    Changes:
    - Converted code from C to C++
    - Changed from backtracking to LALR
    - Generates a state-table
    - Switched to CMake
    
    TODO:
    - Error checking

---
+++ build/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/namikaze/Code/Language_Recognizer/src/lexer.cpp" "src/CMakeFiles/main.dir/lexer.cpp.o" "gcc" "src/CMakeFiles/main.dir/lexer.cpp.o.d"
+  "/home/namikaze/Code/Language_Recognizer/src/main.cpp" "src/CMakeFiles/main.dir/main.cpp.o" "gcc" "src/CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/namikaze/Code/Language_Recognizer/src/parser.cpp" "src/CMakeFiles/main.dir/parser.cpp.o" "gcc" "src/CMakeFiles/main.dir/parser.cpp.o.d"
+  "/home/namikaze/Code/Language_Recognizer/src/stateGenerator.cpp" "src/CMakeFiles/main.dir/stateGenerator.cpp.o" "gcc" "src/CMakeFiles/main.dir/stateGenerator.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
